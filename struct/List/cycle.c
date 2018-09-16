48512e6676b73ef2ecc7c611b976dabd49908376
Lornatang <shiyipaisizuo@gmail.com>
2018-09-16 17:36:16 +0800

    update function

--- struct/List/cycle.c
+++ struct/List/cycle.c
@@ -20,6 +20,57 @@ pNode create() {
 
   for (i = 1; i < length; i++) {
     pNode p = (pNode)malloc(sizeof(node));
+
+    if (p == NULL) {
+      printf("space error!.\n");
+      exit(0);
+    }
+    p->data = data;
+    p->next = L;
+    end->next = p;
+    end = p;
   }
   return L;
 }
+
+//打印链表
+void print(pNode L) {
+  pNode p = L->next;
+  printf("===========Data==========.\n");
+  if (p != NULL) {
+    printf("%d", p->data);
+    p = p->next;
+  }
+  putchar('\n');
+}
+
+//判断链表是否为空
+int isEmpty(pNode L) {
+  if (L->next == L) {
+    printf("is NULL.\n");
+    return -1;
+  } else {
+    printf("not NULL.\n");
+    return 0;
+  }
+}
+
+//计算链表的长度
+int getLength(pNode L) {
+  int length = 0;
+  pNode p = L->next;
+  while (p != L) {
+    length += 1;
+    p = p->next;
+  }
+  return length;
+}
+
+//向链表中插入节点
+int insert(pNode L, int pos, int data);
+
+//从链表中删除节点
+int del(pNode L, int pos);
+
+//删除整个链表，释放内存
+void freeMemory(pNode *p);
