1c15166efe6616281261ac324c64e3bcd99278c1
Lornatang <shiyipaisizuo@gmail.com>
2018-09-16 22:54:20 +0800

    update

--- struct/List/cycle.c
+++ struct/List/cycle.c
@@ -122,3 +122,51 @@ void freeMemory(pNode *p) {
     }
   }
 }
+
+int main(void) {
+  int flag = 0, length = 0;
+  int position = 0, value = 0;
+  pNode head = NULL;
+
+  head = init();
+
+  flag = isEmpty(head);
+  if (flag)
+    printf("单向循环链表为空！\n");
+  else {
+    length = getLength(head);
+    printf("单向循环链表的长度为：%d\n", length);
+    print(head);
+  }
+
+  printf("请输入要插入节点的位置和元素值(两个数用空格隔开)：");
+  scanf("%d %d", &position, &value);
+  flag = insert(head, position, value);
+  if (flag) {
+    printf("插入节点成功！\n");
+    print(head);
+  } else
+    printf("插入节点失败！\n");
+
+  flag = isEmpty(head);
+  if (flag)
+    printf("单向循环链表为空，不能进行删除操作！\n");
+  else {
+    printf("请输入要删除节点的位置：");
+    scanf("%d", &position);
+    flag = del(head, position);
+    if (flag) {
+      printf("删除节点成功！\n");
+      print(head);
+    } else
+      printf("删除节点失败！\n");
+  }
+
+  freeMemory(&head);
+  if (NULL == head)
+    printf("已成功删除单向循环链表，释放内存完成！\n");
+  else
+    printf("删除单向循环链表失败，释放内存未完成！\n");
+
+  return 0;
+}
