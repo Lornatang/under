7254728c6ef1c6101f8139b2ab49bea177102b85
Lornatang <shiyipaisizuo@gmail.com>
2018-09-17 07:17:24 +0800

    update

--- struct/List/list.h
+++ struct/List/list.h
@@ -10,60 +10,17 @@ typedef struct Sequential {
 } seqList;
 
 // Initializes a sequential list with a capacity of 100
-seqList *initList() {
-  seqList *L;
-  L = (seqList *)malloc(sizeof(seqList));
-  L->last = -1;  // The index that the pointer points to is -1
-  return L;
-}
+seqList *initList();
 
 /**
  * Insert data into a sequential linked list.
  */
-int insertList(seqList *L, int i, int x) {
-  if (L->last == MAX_SIZE - 1) {
-    printf("The space is insufficient.\n");
-    return -1;
-  }
-  if (i < 1 || i > L->last + 2) {
-    printf("Index error.\n");
-    return 0;
-  }
-  for (int j = L->last; j > i - 1; j--) {
-    L->data[j + 1] = L->data[j];
-    L->data[i - 1] = x;  // insert data
-    L->last++;           // get last element index
-    return 0;
-  }
-  return 0;
-}
+int insertList(seqList *L, int i, int x);
 
-int delList(seqList *L, int i) {
-  int j = 0;
-  // Check the legality of empty table and delete position
-  if (i < 1 || i > L->last + 1) {
-    printf("data is not exist!.\n");
-    return 0;
-  }
-  for (j = i; j <= L->last; j++) {
-    L->data[j - 1] = L->data[j];  // Move to the previous digit
-  }
-  L->last--;
-  return 0;  // Del success
-}
+int delList(seqList *L, int i);
 
 /**
  * find data is exist
  */
-int find(seqList *L, int x) {
-  int i = 0;
-  while (i <= L->last && L->data[i] != x) {
-    i++;
-  }
-  if (i > L->last)
-    return -1;
-  else
-    return i;
-  return 0;
-}
+int find(seqList *L, int x);
 #endif  // LIST_H
