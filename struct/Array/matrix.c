4bb8c547cc21917c2c7d35e7e4beccaa6ae04025
Lornatang <shiyipaisizuo@gmail.com>
2018-09-24 16:15:42 +0800

    init

--- struct/Array/matrix.c
+++ struct/Array/matrix.c
@@ -1,48 +1,8 @@
+#include "matrix.h"
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define true 1
-#define false 0
-#define error -1
-
-typedef struct {
-  int m;    ///行下标
-  int n;    ///列下标
-  int elm;  ///该下标所保存的元素
-} TTuple;   ///三元组结构
-
-typedef struct {
-  TTuple *tup;  ///三元组顺序表
-  int row;      ///矩阵行数
-  int col;      ///矩阵列数
-  int unul;     ///非 0 元素个数
-} TMatrix;      ///稀疏矩阵结构
-
-///稀疏矩阵方法声明
-TMatrix *CreateEmptyTMatrix(
-    int sizeM, int sizeN);  ///创建一个大小为 sizeM x sizeN 稀疏矩阵
-TMatrix *CreateTMatirxFrom2DArray(void *pArr2D, int sizeM,
-                                  int sizeN);  ///从二维数组中创建稀疏矩阵
-void DestroyTMatrix(TMatrix *pMat);            ///销毁稀疏矩阵
-int ElemLocate(const TMatrix *const pMat, int m,
-               int n);  ///定位矩阵下标 m, n 在稀疏矩阵中的存储位置
-void DisplayTMatrix(const TMatrix *const pMat);  ///输出稀疏矩阵
-int GetTMatrixSize(
-    const TMatrix *const pMat);  ///输出稀疏矩阵 pMat 所占用的空间大小(字节)
-int AppendElem(TMatrix *const pMat, int elm, int m,
-               int n);  ///将元素 elm 添加到稀疏矩阵 m, n 位置
-int DeleteElem(TMatrix *const pMat, int m,
-               int n);  ///删除稀疏矩阵中 m, n 下标指向的元素
-int TMatrixCopy(TMatrix *const pMatDest,
-                TMatrix *const pMatSrc);  ///将稀疏矩阵 pMatSrc 复制到 pMatDest
-int Value(const TMatrix *const pMat, int m, int n,
-          int *pElm);  ///从稀疏矩阵中取下标为 m, n 元素的值
-void ForEach(const TMatrix *const pMat,
-             void (*func)(int *pElm));  ///对矩阵中的每个元素依次执行 func 函数
-
-///稀疏矩阵方法实现
 
 /**
  * @brief 创建一个大小为 sizeM x sizeN 稀疏矩阵
--- struct/Array/matrix.h
+++ struct/Array/matrix.h
@@ -1,8 +1,3 @@
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #define true 1
 #define false 0
 #define error -1
